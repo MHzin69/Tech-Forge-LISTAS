9cfdfc0fd120a447263d18d19a623399b62fbd64
MHzin69 <lohannluna@gmail.com>
2025-03-26 19:42:47 -0300

    @ prim

---
+++ ex1.js
@@ -0,0 +1,4 @@
+const leitura = require('prompt-sync')();
+
+let valor = leitura("Insira um número: ")
+console.log("A metade do número é: "+ (valor/2))
---
+++ ex2.js
@@ -0,0 +1,5 @@
+const leitura = require("prompt-sync")();
+
+let valor =leitura("Insira um valor decimal: ");
+let resultado = valor/5;
+console.log("A divisão desse número por 5 é: "+ resultado)
